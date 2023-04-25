18ddc1d6ddc4eea70a1faa72faae979494e52a1d
tiagokochem <tiagok989@gmail.com>
2023-04-24 21:18:47 -0300

    commit initial conversor

---
+++ conversor/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+const request = "http://api.hgbrasil.com/finance?format=json&key=89c06581";
+
+void main(){
+  runApp(MaterialApp(
+    home: Container()
+  ));
+}
