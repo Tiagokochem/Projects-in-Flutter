6ed1185a79aa9d0a382dbd9d754fa139ef2325f3
tiagokochem <tiagok989@gmail.com>
2023-04-24 21:51:58 -0300

    get async

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+
 
 const request = "http://api.hgbrasil.com/finance?format=json&key=89c06581";
 
-void main(){
+void main() async{
+
+  http.Response response = await http.get(request as Uri);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+
+  print(response.body);
+
   runApp(MaterialApp(
     home: Container()
   ));
