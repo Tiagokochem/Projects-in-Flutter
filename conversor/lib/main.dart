6577dbf279dd380c444a4848c182c74dc219f879
tiagokochem <tiagok989@gmail.com>
2023-04-25 21:18:41 -0300

    AppBar Conversor

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 
@@ -9,12 +9,38 @@ const request = "http://api.hgbrasil.com/finance?format=json&key=89c06581";
 
 void main() async{
 
-  http.Response response = await http.get(request as Uri);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  http.Response response = await http.get(Uri.parse(request));
 
   print(response.body);
 
   runApp(MaterialApp(
-    home: Container()
+    home: Home()
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(Uri.parse(request));
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor\$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: ,
+    );
+  }
 }
