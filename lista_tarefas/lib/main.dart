4cf382393ff215560e510e5ae1e2d9c4a0365478
tiagokochem <tiagok989@gmail.com>
2023-05-08 21:52:26 -0300

    lista de tarefas flutter

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -1,26 +1,24 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  List<Map<String, dynamic>> _toDoList = [];
+  List _toDoList = [];
+  TextEditingController _toDoController = TextEditingController();
 
-  final _toDoController = TextEditingController();
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   @override
   void initState() {
@@ -32,37 +30,31 @@ class _HomeState extends State<Home> {
     });
   }
 
-  void _addTodo() {
+  void _addToDo() {
     setState(() {
-      Map<String, dynamic> newToDo = {
-        "title": _toDoController.text,
-        "ok": false,
-      };
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
       _toDoController.text = "";
+      newToDo["ok"] = false;
       _toDoList.add(newToDo);
       _saveData();
     });
   }
 
-  Future<File> _getFile() async {
-    final directory = await getApplicationDocumentsDirectory();
-    return File("${directory.path}/data.json");
-  }
-
-  Future<File> _saveData() async {
-    String data = json.encode(_toDoList);
-    final file = await _getFile();
-    return file.writeAsString(data);
-  }
-
-  Future<String> _readData() async {
-    try {
-      final file = await _getFile();
-      return await file.readAsString();
-    } catch (e) {
-      print('Ocorreu um erro ao ler o arquivo: $e');
-      return ''; // ou return [] para retornar uma lista vazia
-    }
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+      _saveData();
+    });
+    return null;
   }
 
   @override
@@ -83,42 +75,54 @@ class _HomeState extends State<Home> {
                   child: TextField(
                     controller: _toDoController,
                     decoration: InputDecoration(
-                      labelText: "Nova Tarefa",
-                      labelStyle: TextStyle(color: Colors.blueAccent),
-                    ),
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: _addTodo,
-                  child: Text("Add"),
-                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: _addToDo,
+                )
               ],
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemCount: _toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text(_toDoList[index]["title"]),
-                  value: _toDoList[index]["ok"],
-                  secondary: CircleAvatar(
-                    child: Icon(
-                        _toDoList[index]["ok"] ? Icons.check : Icons.error),
-                  ),
-                  onChanged: (c) {
-                    setState(() {
-                      _toDoList[index]["ok"] = c;
-                      _saveData();
-                    });
-                  },
-                );
-              },
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
             ),
-          ),
+          )
         ],
       ),
     );
   }
-}
+
+  Widget buildItem(BuildContext context, int index) {
+    return Dismissible(
+        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+    direction: DismissDirection.startToEnd,
+    onDismissed: (direction) {
+    setState(() {
+    _lastRemoved = Map.from(_toDoList[index]);
+    _lastRemovedPos = index;
+    _toDoList.removeAt(index);
+    _saveData();
+
+    final snack = SnackBar(
+    content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+    action: SnackBarAction(
+    label: "Desfazer",
+    onPressed: () {
+    setState(() {
+    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+    _saveData();
+    });
+    }),
+    duration: Duration(seconds: 2),
+    );
+    Scaffold
